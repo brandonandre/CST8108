678ae8d9e53cbdc928de3a00d1c1448c3dfa9eb1
Brandon Andre <dev.brandonandre@gmail.com>
2017-03-28 13:41:07 -0400

    Lab 11 Client Side

---
+++ Lab11/src/SimpleClient.java
@@ -0,0 +1,22 @@
+
+// SimpleClient.java: A simple client program. import java.net.*;
+import java.io.*;
+import java.net.*;
+
+public class SimpleClient {
+	public static void main(String args[]) throws IOException {
+		// Open your connection to a server, at port 1254
+		Socket s1 = new Socket("192.168.1.103", 1254);
+		
+		// Get an input file handle from the socket and read the input 
+		InputStream s1In = s1.getInputStream();
+		DataInputStream dis = new DataInputStream(s1In);
+		String st = new String(dis.readUTF());
+		System.out.println(st);
+		
+		// When done, just close the connection and exit
+		dis.close();
+		s1In.close();
+		s1.close();
+	}
+}
